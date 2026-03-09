3ad816344150f01814cd105df29344af2dea6625
cbhv4321 <cbhv1029@gmail.com>
2026-03-09 10:04:11 +0200

    added stage 2

---
+++ frontend/hero-manager-frontend/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HerosDisplayPage } from './heros-display-page/heros-display-page';
+
+@Component(
+{
+  selector: 'app-root',
+  standalone: true,
+  imports: [HerosDisplayPage],
+  templateUrl: './app.html'
+})
+export class AppComponent
+{
+  
+}
